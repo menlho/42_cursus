6a5cbe569438d5bb53e882bbad430662fa9060f0
= <=>
2023-05-21 17:22:48 +0200

    fix strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -10,45 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/*#include "libft.h"
-
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-	unsigned int	lendest;
-	unsigned int	lenstr;
-	unsigned int	i;
-
-	lendest = ft_strlen(dst);
-	lenstr = ft_strlen(src);
-	i = 0;
-	if (dst == NULL)
-		return (ft_strlen(src));
-	if (!(dst) && !(src) && (dstsize == 0))
-		return (0);
-	if ((dstsize - 1 < lendest) || (dstsize == 0))
-		return (lenstr + dstsize);
-	while (((unsigned char) src[i]) && (lendest + i < dstsize - 1))
-	{
-		dst[lendest + i] = (unsigned char) src[i];
-		i++;
-	}
-	dst[lendest + i] = '\0';
-	return ((unsigned int)(lendest + ft_strlen(src)));
-}
-*/
-
 #include "libft.h"
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
-	char	*d;
+	char	*buff;
 
 	if (!dst && !dstsize)
 		return (0);
 	i = 0;
-	d = dst;
-	while (*d++ && i < dstsize)
+	buff = dst;
+	while (*buff++ && (i < dstsize))
 		i++;
 	if (i == dstsize)
 		return (i + ft_strlen(src));
