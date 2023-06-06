91869aa1eba1f41e511f49390325f9fb285fc049
Morgan Masclet <menlho.83@gmail.com>
2023-06-06 11:19:49 +0200

    protected and polished strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,51 +6,46 @@
 /*   By: momascle <momascle@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 06:08:00 by momascle          #+#    #+#             */
-/*   Updated: 2023/06/06 07:19:37 by momascle         ###   ########.fr       */
+/*   Updated: 2023/06/06 11:13:34 by momascle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
 
-static unsigned int isinset(const char *set, char c)
+static int isinset(const char *set, char c)
 {
+	int		i;
 
-	while (*set)
+	i = 0;
+	while (set[i])
 	{
-		if (*set++ == c)
+		if (set[i++] == c)
 			return (1);
 	}
 	return (0);
 }
 char	*ft_strtrim(const char *s1,const char *set)
 {
-	unsigned int	i;
-	unsigned int	j;
-	unsigned int	start;
-	unsigned int	end;
+	int	i;
+	int	start;
+	int	end;
 	char	*trimmed;
 
+	if (!s1)
+		return (ft_calloc(1, sizeof(char *)));
 	i = 0;
 	while (isinset(set, s1[i]))
 		i++;
 	start = i;
-	while (s1[i])
-	{
-		j = i;
-		while (isinset(set, s1[j]))
-		{
-			if (s1[j] == '\0')
-				end = j - i;
-			j++;
-		}
-		if (s1[i + 1] == '\0')
-			end = i;
-		i++;
-	}
-	trimmed = (char *)malloc(sizeof(char) * ((end - start) + 1));
-	ft_strlcpy(trimmed, s1 + start, (end - start) + 1);
+	end = ft_strlen(s1);
+	if (end < 0)
+		return(ft_strdup(""));
+	while (s1[end - 1] && isinset(set, s1[end - 1]))
+		end--;
+	trimmed = ft_substr(s1, start, end - start);
 	return (trimmed);
+
 }
 
 /*int main()
